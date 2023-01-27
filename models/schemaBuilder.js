e74fc0873dc25f1ab6a8b2164ff73062a6c840fe
Jose Toscano <jtoscano@softtouchpos.com>
2023-01-27 20:50:55 -0300

    remove version key of model

--- models/schemaBuilder.js
+++ models/schemaBuilder.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose"),
     Schema = mongoose.Schema
 
 module.exports = (structure) => {
-    var myschema = new Schema(structure)
+    var myschema = new Schema(structure, { versionKey: false })
     myschema.plugin(require('mongoose-paginate'))
     myschema.methods.toJSON = function () {
         var obj = this.toObject()
