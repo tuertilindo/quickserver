e69a7d13e5a6444d5df55bc88e6e303716ed6ce6
Jose Maria Toscano <tuertilindo@gmail.com>
2024-08-13 17:48:17 -0300

    adding messages logs

--- index.js
+++ index.js
@@ -12,16 +12,21 @@ exports.init = (params, load) => {
     const pk = process.env.PRIVATE_KEY || params.privatekey
     if (!pk) throw Error("The Private key is not set, please add it to enviorement: PRIVATE_KEY ")
     app.set('privateKey', pk)
+    console.log('configuring...')
     require("./configs/config")(app, params)
+    console.log('Loading app...')
     if (load) load(app)
 
     const port = params.port || 8080
     if (!params.noserve) {
+        console.log('starting server...')
         server.listen(port, function () {
             console.log("Quickserver running on http://localhost:" + port)
         })
+    } else {
+        console.log('serving is set to false...')
     }
-
+    console.log('Quickserver initialized!')
     return app
 }
 exports.helper = {
